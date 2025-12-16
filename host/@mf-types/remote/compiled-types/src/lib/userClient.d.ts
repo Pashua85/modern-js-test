3a28bfa6e2253c8a4b347562dca8bf5f8cc0bc4e
Павел Лашук <p.lashuk@takeoff-staff.ru>
2025-12-16 15:30:10 +0500

    update type

--- host/@mf-types/remote/compiled-types/src/lib/userClient.d.ts
+++ host/@mf-types/remote/compiled-types/src/lib/userClient.d.ts
@@ -1,8 +1,6 @@
 export declare class UserClient {
     private cache;
     private pending;
-    private key;
-    peekUserName(userId: string): string | undefined;
     getUserName(userId: string, lang: string): Promise<string>;
     private loadUserName;
 }
